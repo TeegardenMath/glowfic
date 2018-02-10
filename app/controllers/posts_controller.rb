bc2b4b38d67515d794f62f1b3b3ed35847513c4e
Teceler <19716939+teceler@users.noreply.github.com>
2018-02-10 12:45:58 -0600

    Add written_params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -478,10 +478,6 @@ class PostsController < WritableController
       :privacy,
       :subject,
       :description,
-      :content,
-      :character_id,
-      :icon_id,
-      :character_alias_id,
       :authors_locked,
       :audit_comment,
       :private_note,
@@ -496,5 +492,15 @@ class PostsController < WritableController
     end
 
     params.fetch(:post, {}).permit(allowed_params)
+  end
+
+  def written_params
+    params.fetch(:reply, {}).permit(
+      :content,
+      :character_id,
+      :icon_id,
+      :character_alias_id,
+      :audit_comment,
+    )
   end
 end
