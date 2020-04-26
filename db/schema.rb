f9a23eca7ab0bf8d359f7dbea8e6155971c09d97
Marri Nikari <xxstealthestarsxx@gmail.com>
2020-04-25 22:47:07 -0400

    Did not press save apparently

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2020_04_16_173619) do
-=======
-ActiveRecord::Schema.define(version: 2020_04_08_132110) do
->>>>>>> Begin notifications
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
