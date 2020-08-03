3d844adc136e2cc713172cc7112c30e9624292d9
teceler <19716939+teceler@users.noreply.github.com>
2020-08-03 13:07:58 -0500

    Add a skip_written variable for testing purposes

--- app/models/post.rb
+++ app/models/post.rb
@@ -33,7 +33,7 @@ class Post < ApplicationRecord
 
   has_one :written, -> { where(reply_order: 0) }, class_name: 'Reply', inverse_of: :post
 
-  attr_accessor :is_import
+  attr_accessor :is_import, :skip_written
   attr_writer :skip_edited
 
   validates :subject, presence: true
@@ -346,6 +346,7 @@ class Post < ApplicationRecord
   end
 
   def create_written
+    return if skip_written
     self.written = Reply.create!(
       post: self,
       reply_order: 0,
