d0a411f6bce19ddcdcb3c80d365364085efdd8d5
teceler <19716939+teceler@users.noreply.github.com>
2021-05-18 21:58:45 -0500

    Fixes

--- app/assets/javascripts/tags/edit.js
+++ app/assets/javascripts/tags/edit.js
@@ -1,5 +1,5 @@
 /* global createTagSelect */
 $(document).ready(function() {
-  var tagID = $("#tag_parent_setting_ids").data('tag-id');
+  var tagID = $("#setting_parent_setting_ids").data('tag-id');
   createTagSelect("Setting", "parent_setting", "setting", {tag_id: tagID});
 });
