5b6c424a6b008ee693597f237a7496c1838352c8
teceler <19716939+teceler@users.noreply.github.com>
2022-10-11 21:19:00 -0500

    Standardize on find_parent

--- app/controllers/aliases_controller.rb
+++ app/controllers/aliases_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class AliasesController < ApplicationController
   before_action :login_required
-  before_action :find_character
+  before_action :find_parent
   before_action :require_permission
   before_action :find_model, only: :destroy
 
@@ -45,7 +45,7 @@ class AliasesController < ApplicationController
 
   private
 
-  def find_character
+  def find_parent
     return if (@character = Character.find_by(id: params[:character_id]))
     flash[:error] = "Character could not be found."
     redirect_to user_characters_path(current_user)
